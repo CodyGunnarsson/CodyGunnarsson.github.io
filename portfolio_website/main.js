572b3a507f026bbfab82cf48ae83edc1464bbea1
Cody Gunnarsson <89118369+CodyGunnarsson@users.noreply.github.com>
2022-12-26 10:31:40 -0700

    Update main.js

--- portfolio_website/main.js
+++ portfolio_website/main.js
@@ -2,7 +2,7 @@ const body = document.querySelector("body");
 
 
 function star() {
-    let meteorNumber = 10;
+    let meteorNumber = 25;
         for (let i = 1; i <= meteorNumber; i++) {
         body.innerHTML += `<div class="meteor-${i}"></div>`;
     }
